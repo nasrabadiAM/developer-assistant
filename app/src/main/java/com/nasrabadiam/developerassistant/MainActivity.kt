2b0ac13a6a33cb0f1405325a43a1c04018e05e8a
Ali Nasrabadi <nasrabadiam@gmail.com>
2018-12-02 11:59:28 +0330

    migrate to support libs to androidx

--- app/src/main/java/com/nasrabadiam/developerassistant/MainActivity.kt
+++ app/src/main/java/com/nasrabadiam/developerassistant/MainActivity.kt
@@ -18,21 +18,14 @@
 
 package com.nasrabadiam.developerassistant
 
-import android.support.design.widget.TabLayout
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.ViewPager
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
